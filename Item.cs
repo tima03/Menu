c56f2fe97d277079a88cf61c983b9c8250fb7220
Lera <tima-psp2002@yandex.ru>
2023-02-17 01:16:14 +0300

    Добавьте файлы проекта.

---
+++ Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMenu
+{
+    public class Item
+    {
+        string name;
+        string Funciton;
+
+        public Item() {
+            this.name = "NULL";
+            this.Funciton = "NULL";
+        }
+        public Item(string name,string Function)
+        {
+            this.name = name;
+            this.Funciton = name;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using CMenu;
+
+class Program {
+static public void Main(string[] args)
+    {
+       List<Item> ListMain = new List<Item>();
+       ListMain.Add(new Item("Auth","AuthExe"));
+       ListMain.Add(new Item("Exit", "ExitExe"));
+       CMenu.CMenu Menu = new CMenu.CMenu("Shop",ListMain);
+       Menu.run();
+    }
+}
