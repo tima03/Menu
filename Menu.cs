42c0e522505671bfe5bb977b15e962bf381f3788
Алексей <50240977+inozemcev163@users.noreply.github.com>
2023-02-17 03:06:55 +0400

    Создан класс Cmenu и следующие методы: -SetName -SetList -GetName -GetItems -Print(ещё не работает!!!!!)

--- Menu.cs
+++ Menu.cs
@@ -8,6 +8,47 @@ namespace CMenu
 {
     internal class CMenu
     {
+        private string name;
+        private int pointer;
+        private List<Item>? ItemMenu = new List<Item>();
 
+        public CMenu(string Name, List<Item> ItemMenu)
+        {
+            this.name = Name;
+            this.pointer = 0;
+            this.ItemMenu = ItemMenu;
+        }
+
+        public CMenu()
+        {
+            this.name = "NULL";
+            this.pointer = 0;
+            this.ItemMenu = null;
+        }
+
+        public void SetName(string Name)
+        {
+            this.name = Name;
+        }
+
+        public void SetList(List<Item> List)
+        {
+            this.ItemMenu = List;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public List<Item> GetItems()
+        {
+            return ItemMenu;
+        }
+
+        public void Print()
+        {
+            // Ещё не доделано
+        }
     }
 }
