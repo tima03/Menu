737f99919b9e2a44b5e7d93dc3b7711519be851c
Lera <tima-psp2002@yandex.ru>
2023-02-17 02:05:25 +0300

    - Добавленн конструктор поумолчанию Item() - Добавленн конструктор c  параметрами Item(string name,string function) - Добавленны сеттеры и геттеры для полей name и function: SetFunction, SetName, GetName, GetFunciton - Добавлены функции печати полей в консоль для name и function: Print, PrintName, PrintFunction

--- Item.cs
+++ Item.cs
@@ -9,16 +9,45 @@ namespace CMenu
     public class Item
     {
         string name;
-        string Funciton;
+        string funciton;
 
         public Item() {
             this.name = "NULL";
-            this.Funciton = "NULL";
+            this.funciton = "NULL";
         }
-        public Item(string name,string Function)
+        public Item(string name,string function)
         {
             this.name = name;
-            this.Funciton = name;
+            this.funciton = function;
+        }
+
+        public void SetFunction(string funciton) {
+            this.funciton = funciton;
+        }
+        
+        public void SetName(string name) {
+        this.name = name;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+        public string GetFunciton()
+        {
+            return this.funciton;
+        }
+
+        public void Print() {
+        Console.WriteLine("Имя элемента меню: {0} Название выполняемой ф-ии: {1}",this.name,this.funciton);
+        }
+        public void PrintName()
+        {
+            Console.WriteLine("Имя элемента меню: {0}", this.name);
+        }
+        public void PrintFunction()
+        {
+            Console.WriteLine("Название выполняемой ф-ии: {0}", this.funciton);
         }
 
     }
